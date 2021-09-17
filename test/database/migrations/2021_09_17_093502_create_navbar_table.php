a5ca96e92778073106470409314a792f84dfce9f
74WINXD <ccw.la@outlook.com>
2021-09-17 11:38:19 +0200

    Ik heb een migration en een model aangemaakt (nog niet helemaal afgerond)

---
+++ test/database/migrations/2021_09_17_093502_create_navbar_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNavbarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('navbar', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('route');
+            $table->integer('ordering')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('navbar');
+    }
+}
