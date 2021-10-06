46673ee7abcf682973c93418d40b0cde963d4c5b
74WINXD <ccw.la@outlook.com>
2021-10-06 20:43:00 +0200

    Ik kan mijn dat data updaten

--- test/resources/views/Element/edit-elements.blade.php
+++ test/resources/views/Element/edit-elements.blade.php
@@ -18,25 +18,25 @@
                     </div>
                     <div class="card-body">
 
-                        <form action="{{ url('update-element/'.$element->id) }}" method="POST">
+                        <form action="{{ url('update-elements/'.$element->id) }}" method="POST">
                             @csrf
                             @method('PUT')
 
                             <div class="form-group mb-3">
                                 <label for="">elementname</label>
-                                <input type="text" name="name" value="{{$element->elementname}}" class="form-control">
+                                <input type="text" name="elementname" value="{{$element->elementname}}" class="form-control">
                             </div>
                             <div class="form-group mb-3">
                                 <label for="">element type</label>
-                                <input type="text" name="email" value="{{$element->elementtype}}" class="form-control">
+                                <input type="text" name="elementtype" value="{{$element->elementtype}}" class="form-control">
                             </div>
                             <div class="form-group mb-3">
                                 <label for="">element img</label>
-                                <input type="text" name="course" value="{{$element->elementimg}}" class="form-control">
+                                <input type="text" name="elementimg" value="{{$element->elementimg}}" class="form-control">
                             </div>
                             <div class="form-group mb-3">
                                 <label for="">element lore</label>
-                                <input type="text" name="section" value="{{$element->elementlore}}" class="form-control">
+                                <input type="text" name="elementlore" value="{{$element->elementlore}}" class="form-control">
                             </div>
                             <div class="form-group mb-3">
                                 <button type="submit" class="btn btn-primary">Update Element</button>
